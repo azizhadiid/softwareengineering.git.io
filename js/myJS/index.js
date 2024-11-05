129f3ef4ce30e594957e8ff7d2f32422c5edabe4
azizhadid <azizalhadiid88@gmail.com>
2024-11-05 23:10:12 +0700

    update

---
+++ js/myJS/index.js
@@ -0,0 +1,36 @@
+function toggleLinknyaClass() {
+    const navLinks = document.querySelectorAll('.navbar-nav .nav-tengah');
+    const btn = document.querySelector('.tombol');
+    const logo = document.querySelector('.puskeSmart');
+    const cont1 = document.querySelector('.csc1');
+    const card = document.querySelectorAll('.card');
+  
+    if (window.innerWidth < 992) {
+        // Aktifkan tampilan mobile
+        navLinks.forEach(link => {
+            if (link.classList.contains('linknya')) {
+                link.classList.remove('linknya');
+                link.classList.add('link-hp');
+            }
+        });
+        btn.style.marginTop = '20px';
+        logo.style.width = '50px';
+        logo.style.height = '50px';
+        if (cont1) cont1.style.marginTop = '50px';
+        card.style.width = '300px';
+    } else {
+        // Aktifkan tampilan desktop
+        navLinks.forEach(link => {
+            if (link.classList.contains('link-hp')) {
+                link.classList.add('linknya');
+                link.classList.remove('link-hp');
+            }
+        });
+        logo.style.width = '70px'; // Sesuaikan ukuran logo jika dibutuhkan
+        logo.style.height = '70px';
+        btn.style.marginTop = '0px';
+    }
+}
+
+toggleLinknyaClass();
+window.addEventListener('resize', toggleLinknyaClass);
